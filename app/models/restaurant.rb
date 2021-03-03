fd4382b533ab5e8c9f2fe7226a539673b9fd9a8e
andreamazzatxt <andreamazza89@gmail.com>
2021-03-03 14:13:38 +0100

    Include add review form in restaurant show page

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -4,4 +4,8 @@ class Restaurant < ApplicationRecord
   validates :name, presence: true
   validates :address, presence: true
   validates :category, inclusion: { in: TYPES }
+  before_save :default_values
+  def default_values
+    self.phone_number = 'n\a' if self.phone_number.nil?
+  end
 end
